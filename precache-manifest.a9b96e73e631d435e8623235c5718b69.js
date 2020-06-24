1f8328f77f5ef40d24fad291b975a7d64096c44f
faisal95bd <faisal95bd@gmail.com>
2020-06-24 23:43:20 +0600

    Updates

---
+++ precache-manifest.a9b96e73e631d435e8623235c5718b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1a1bbe5381771e9a9a8600830fad17",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "a814fd47562a680e6097",
+    "url": "/todoapp/static/css/main.5b369b08.chunk.css"
+  },
+  {
+    "revision": "fcc97786fd336639308e",
+    "url": "/todoapp/static/js/2.a639893d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.a639893d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a814fd47562a680e6097",
+    "url": "/todoapp/static/js/main.08e16ba6.chunk.js"
+  },
+  {
+    "revision": "fce373094848ebf64ee3",
+    "url": "/todoapp/static/js/runtime-main.7766f948.js"
+  }
+]);
