7c00325ee028b5203850d9d5e73d3d7d6f14ed22
faisal95bd <faisal95bd@gmail.com>
2025-11-29 23:42:30 +0600

    refactor: update dependencies and migrate class component to functional component
    - Updated dependencies in package.json to latest versions for better performance and security.
    - Refactored App component from class-based to functional component using React hooks (useState).
    - Simplified state management and event handling for adding and deleting todo items.
    - Updated rendering logic to reflect changes in state more efficiently.
    - Modified index.js to use createRoot for rendering the App component.

--- src/App.js
+++ src/App.js
@@ -1,106 +1,84 @@
-import React from 'react';
-import './App.css';
-import Logo from './icons8-to-do-100.png';
+import React, { useState } from "react";
+import "./App.css";
+import Logo from "./icons8-to-do-100.png";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newItem: '',
-      list: [],
-    };
-  }
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [list, setList] = useState([]);
 
   // method for adding an item
-
-  addItem(todoValue) {
-    if (todoValue !== '') {
-      const newItem = {
+  const addItem = (todoValue) => {
+    if (todoValue !== "") {
+      const newItemObj = {
         id: Date.now(),
         value: todoValue,
         isDone: false,
       };
 
-      const list = [...this.state.list];
-      list.push(newItem);
-
-      this.setState({
-        list,
-        newItem: '',
-      });
+      setList([...list, newItemObj]);
+      setNewItem("");
     }
-  }
+  };
 
-  // method for deleting an ittem
-
-  delItem(id) {
-    const list = [...this.state.list];
+  // method for deleting an item
+  const delItem = (id) => {
     const updatedList = list.filter((item) => item.id !== id);
-    this.setState({
-      list: updatedList,
-    });
-  }
+    setList(updatedList);
+  };
 
-  updateInput(input) {
-    this.setState({
-      newItem: input,
-    });
-  }
+  const updateInput = (input) => {
+    setNewItem(input);
+  };
 
-  render() {
-    return (
-      <div>
-        <img className="logo" src={Logo} alt="todo logo" />
-        <h1 className="app-title"> My ToDo List</h1>
-        <div className="container">
-          Add todo... <br />
-          <input
-            type="text"
-            className="text-field"
-            placeholder="write a todo"
-            required
-            value={this.state.newItem}
-            onChange={(i) => this.updateInput(i.target.value)}
-          ></input>
-          <button
-            className="add-btn"
-            onClick={() => this.addItem(this.state.newItem)}
-            disabled={!this.state.newItem.length}
-          >
-            Add Todo
-          </button>
-          <div className="list">
-            <ul>
-              {this.state.list.map((item) => {
-                return (
-                  <li key={item.id}>
-                    {/* <input
+  return (
+    <div>
+      <img className="logo" src={Logo} alt="todo logo" />
+      <h1 className="app-title"> My ToDo List</h1>
+      <div className="container">
+        Add todo... <br />
+        <input
+          type="text"
+          className="text-field"
+          placeholder="write a todo"
+          required
+          value={newItem}
+          onChange={(i) => updateInput(i.target.value)}
+        />
+        <button
+          className="add-btn"
+          onClick={() => addItem(newItem)}
+          disabled={!newItem.length}
+        >
+          Add Todo
+        </button>
+        <div className="list">
+          <ul>
+            {list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {/* <input
                       type="checkbox"
                       name="isDone"
                       checked={item.isDone}
                       onChange={() => {}}
                     /> */}
-                    {item.value}
-                    <button
-                      className="del-btn"
-                      onClick={() => this.delItem(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                );
-              })}
-              {/* <li>
+                  {item.value}
+                  <button className="del-btn" onClick={() => delItem(item.id)}>
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+            {/* <li>
                 <input type="checkbox" />
                 Learn React
                 <button className="del-btn"> Delete</button>
               </li> */}
-            </ul>
-          </div>
+          </ul>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
